db27ed68ad2183d4d9c85a6b55b41a0190ac4bfe
Vlatko Kosturjak <kost@linux.hr>
2020-03-15 18:17:03 +0100

    initial import of openFPGALoader

---
+++ openfpgaloader.rb
@@ -0,0 +1,19 @@
+class OpenFPGALoader < Formula
+  desc "Universal utility for programming FPGA"
+  homepage "https://github.com/trabucayre/openFPGALoader"
+  head "https://github.com/trabucayre/openFPGALoader"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libftdi"
+  depends_on "libusb"
+  depends_on "argp-standalone"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+end
+
+
